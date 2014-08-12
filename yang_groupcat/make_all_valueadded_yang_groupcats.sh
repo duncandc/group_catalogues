d6647a3c04f94a845924e66729f4313ec02cf28c
duncandc <duncan.campbell@yale.edu>
2014-08-12 11:19:35 -0700

    Update make_all_valueadded_yang_groupcats.sh

--- yang_groupcat/make_all_valueadded_yang_groupcats.sh
+++ yang_groupcat/make_all_valueadded_yang_groupcats.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python make_valueadded_yang_groupcat_2.py sample1_L_model &
-python make_valueadded_yang_groupcat_2.py sample2_L_model &
-python make_valueadded_yang_groupcat_2.py sample3_L_model &
+python make_valueadded_yang_groupcat_L.py sample1_L_model &
+python make_valueadded_yang_groupcat_L.py sample2_L_model &
+python make_valueadded_yang_groupcat_L.py sample3_L_model &
 
-python make_valueadded_yang_groupcat_1.py sample1_M_model &
-python make_valueadded_yang_groupcat_1.py sample2_M_model &
-python make_valueadded_yang_groupcat_1.py sample3_M_model &
+python make_valueadded_yang_groupcat_M.py sample1_M_model &
+python make_valueadded_yang_groupcat_M.py sample2_M_model &
+python make_valueadded_yang_groupcat_M.py sample3_M_model &
