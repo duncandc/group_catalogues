1a8cb7c2177716e5da87959f636a8b7d5a7b2770
duncandc <duncan.campbell@yale.edu>
2014-08-12 11:33:28 -0700

    Update read_all_tinker_mock_groups.sh

--- tinker_groupcat/mock_runs/read_all_tinker_mock_groups.sh
+++ tinker_groupcat/mock_runs/read_all_tinker_mock_groups.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python read_tinker_groups_2.0.py 0 &
-python read_tinker_groups_2.0.py 1 &
-python read_tinker_groups_2.0.py 2 &
-python read_tinker_groups_2.0.py 3 &
-python read_tinker_groups_2.0.py 4 & 
-python read_tinker_groups_2.0.py 5 &
-python read_tinker_groups_2.0.py 6 &
+python read_tinker_groups.py 0 &
+python read_tinker_groups.py 1 &
+python read_tinker_groups.py 2 &
+python read_tinker_groups.py 3 &
+python read_tinker_groups.py 4 & 
+python read_tinker_groups.py 5 &
+python read_tinker_groups.py 6 &
