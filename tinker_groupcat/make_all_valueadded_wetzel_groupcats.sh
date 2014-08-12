157b03c64f4ef8f891bdb50620c5a9c5a6b7065f
Duncan Campbell <duncan.campbell@yale.edu>
2014-08-12 15:44:45 -0400

    adding more files

---
+++ tinker_groupcat/make_all_valueadded_wetzel_groupcats.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python make_valueadded_wetzel_groupcat_1.py 9.7 &
+python make_valueadded_wetzel_groupcat_1.py 10.1 &
+python make_valueadded_wetzel_groupcat_1.py 10.6 &
+
+python make_valueadded_wetzel_groupcat_2.py 19  &
