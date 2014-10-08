7a946a39ccd5b9d484b965a6ccdf83b45d3073f7
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-10-08 11:58:48 -0400

    small change

--- yang_groupcat/mock_runs/process_all_yang_mockruns.sh
+++ yang_groupcat/mock_runs/process_all_yang_mockruns.sh
@@ -3,8 +3,8 @@
 #python process_yang_mockruns_2.0.py Mr19_age_distribution_matching_mock 1 &
 #python process_yang_mockruns_2.0.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle 1 &
 
-python process_yang_mockruns_2.0.py Mr19_age_distribution_matching_mock 2 &
-python process_yang_mockruns_2.0.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle 2 &
+python process_yang_mockruns.py Mr19_age_distribution_matching_mock 2 &
+python process_yang_mockruns.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle 2 &
 
 #python process_yang_mockruns_2.0.py Mr19_age_distribution_matching_mock 3 &
 #python process_yang_mockruns_2.0.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle 3 &
