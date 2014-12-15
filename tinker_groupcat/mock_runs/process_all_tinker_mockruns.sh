c265e38d5a95293346f056fbe27fdbd0225728e6
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-12-15 16:48:12 -0700

    small changes

---
+++ tinker_groupcat/mock_runs/process_all_tinker_mockruns.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python process_all_tinker_mockruns.py 0 &
+python process_all_tinker_mockruns.py 1 &
+python process_all_tinker_mockruns.py 2 &
+python process_all_tinker_mockruns.py 3 &
+python process_all_tinker_mockruns.py 4 & 
+python process_all_tinker_mockruns.py 5 &
+python process_all_tinker_mockruns.py 6 &
