3d596c7d28e07e3f094b9c277054e225443adf91
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-12-15 16:43:11 -0700

    small change

---
+++ yang_groupcat/mock_runs/read_all_yang_mockruns.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#version 1
+python read_yang_mockruns.py 0 1 
+python read_yang_mockruns.py 1 1 
+
+#version 2
+python read_yang_mockruns.py 0 2 
+python read_yang_mockruns.py 1 2 
+
+#version 3
+python read_yang_mockruns.py 0 3 
+python read_yang_mockruns.py 1 3 
+
+#version 4
+python read_yang_mockruns.py 0 4 
+python read_yang_mockruns.py 1 4 
+
+#version 5
+python read_yang_mockruns.py 0 5 
+python read_yang_mockruns.py 1 5 
+
